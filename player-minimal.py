146d50cda3a0d5a81b2dd7f368646004bf8e08f4
Hadrien Titeux <hadwarez@gmail.com>
2014-12-28 02:07:27 +0100

    Porting the minimal player to Gst1.0é

--- player-minimal.py
+++ player-minimal.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-import pygst
-pygst.require('0.10')
-import gst
-import gobject
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst, Gtk
 import os
 
-mainloop = gobject.MainLoop()
+Gst.init()
+mainloop = GObject.MainLoop()
+
 #setting up a single "playbin" element which handles every part of the playback by itself
-pl = gst.element_factory_make("playbin", "player")
+pl = Gst.ElementFactory.make("playbin", "player")
 pl.set_property('uri','file://'+os.path.abspath('/tmp/lea.mp3'))
 
 #running the playbin 
-pl.set_state(gst.STATE_PLAYING)
+pl.set_state(Gst.State.PLAYING)
 mainloop.run()
